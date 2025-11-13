c19e1557ee327d564f727f084d6ac3417fb39a46
rashel9255 <ibrahim2000167@gmail.com>
2025-11-13 17:46:19 +0600

    deploy to firebase

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db(process.env.DB_NAME);
         const courseCollection = db.collection("courses");
@@ -193,7 +193,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("MongoDB connected successfully!");
     } finally {
     }
